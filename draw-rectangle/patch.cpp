eec8673beeb4b62a28db39a9d577e7b2a1fdcced
Matt <mw283@cam.ac.uk>
2019-11-11 23:42:23 +0000

    modified:   draw-circle/patch.xodp 	new file:   draw-rectangle/patch.cpp 	modified:   draw-rectangle/patch.xodp

---
+++ draw-rectangle/patch.cpp
@@ -0,0 +1,24 @@
+
+struct State {
+};
+
+{{ GENERATED_CODE }}
+
+void evaluate(Context ctx) {
+    // The node responds only if there is an input pulse
+    if (!isInputDirty<input_UPD>(ctx))
+        return;
+
+    // Get a pointer to the `TVout` class instance
+    auto tv = getValue<input_DEV>(ctx);
+    uint8_t x = getValue<input_X>(ctx);
+    uint8_t y = getValue<input_Y>(ctx);
+    uint8_t w = getValue<input_W>(ctx);
+    uint8_t h = getValue<input_H>(ctx);
+    char color = getValue<input_COLOR>(ctx);
+    char fill = getValue<input_FILL>(ctx);
+
+    tv->draw_rect(x, y, w, h, color, fill);
+
+    emitValue<output_DONE>(ctx, 1);
+}
