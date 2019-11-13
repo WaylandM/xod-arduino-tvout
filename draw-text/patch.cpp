9574cf4158b7f8310c2d867f87a113ba38a7c915
Matt <mw283@cam.ac.uk>
2019-11-13 19:20:03 +0000

    modified:   draw-text/patch.cpp 	modified:   draw-text/patch.xodp

--- draw-text/patch.cpp
+++ draw-text/patch.cpp
@@ -11,20 +11,41 @@ void evaluate(Context ctx) {
 
     // Get a pointer to the `TVout` class instance
     auto tv = getValue<input_DEV>(ctx);
+    auto xString = getValue<input_TEXT>(ctx);
+    auto font = getValue<input_FONT>(ctx);
     uint8_t x = getValue<input_X>(ctx);
     uint8_t y = getValue<input_Y>(ctx);
-    uint8_t w = getValue<input_W>(ctx);
-    uint8_t h = getValue<input_H>(ctx);
-    char color = getValue<input_COLOR>(ctx);
-    char fill = getValue<input_FILL>(ctx);
+    auto xy = getValue<input_XY>(ctx);
 
+    int N=length(xString) + 1;
+    char cString[N];
+    for(int i=0;i<N;i++)
+    cString[i]=0;
+    // move the data from XString to the plain C string
+    dump(xString, cString);
 
-    tv->select_font(font4x6);
-    tv->select_font(font6x8);
-    tv->select_font(font8x8);
-    tv->select_font(font8x8ext);
+    if (font==0) {
+        tv->select_font(font4x6);
+    }
+    else if(font==1) {
+        tv->select_font(font6x8);
+    }
+    else if(font==2) {
+        tv->select_font(font8x8);
+    }
+    else if(font==3) {
+        tv->select_font(font8x8ext);
+    }
+    else {
+      raiseError(ctx);
+      return;
+    }
     
-    tv->draw_rect(x, y, w, h, color, fill);
+    if (xy) {
+        tv->println(x, y, cString)
+    } else {
+        tv->println(cString)
+    }
 
     emitValue<output_DONE>(ctx, 1);
 }
